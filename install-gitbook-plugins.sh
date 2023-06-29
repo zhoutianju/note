8ef5f6d8ea20351ab59072007be8562f696834e4
zhoutianju <tianju.zhou@gmail.com>
2023-06-29 18:07:51 +0800

    增加：初始安装 gitboot 插件脚本

---
+++ install-gitbook-plugins.sh
@@ -0,0 +1,13 @@
+npm install gitbook-plugin--lunr
+npm install gitbook-plugin--search
+npm install gitbook-plugin-search-plus
+npm install gitbook-plugin--sharing
+npm install gitbook-plugin-github@2.0.0
+npm install gitbook-plugin-mermaid-gb3
+npm install gitbook-plugin-favicon
+npm install gitbook-plugin-hide-element
+npm install gitbook-plugin-signature
+npm install gitbook-plugin-code
+npm install gitbook-plugin-chapter-fold
+npm install gitbook-plugin-anchor-navigation-ex
+
